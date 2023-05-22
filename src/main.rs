514956914525998573d5a1be2c07853fcb1ad74f
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-05-22 10:11:11 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use pcap::Device;
+use pcap::{Device, Capture};
 
 fn main() {
+    println!("Starting Packet Capture");
+    let device = Device::lookup()
+        .expect("Device lookup Failed")
+        .expect("No Device available");
+    println!("Using device {}", device.name);
 
-    let mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
-    while let Ok(packet) = cap.next_packet() {
-        println!("received packet! {}", packet);
-    }
+    let mut cap = Capture::from_device(device)
+        .unwrap()
+        .immediate_mode(true)
+        .open()
+        .unwrap();
 }
