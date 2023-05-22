7c21e9e15f5d2ae2bf0cbc447971ee528d7e8903
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-05-22 10:51:56 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use std::io;
 use pcap::{Device, Capture};
 
 fn main() {
-    println!("Starting Packet Capture");
-    let device = Device::lookup()
-        .expect("Device lookup Failed")
-        .expect("No Device available");
-    println!("Using device {}", device.name);
+    //find all devices installed
+    let mut devices = [];
+    let mut i: u32 = 0;
+    println!("Select a device:");
+    for device in Device::list().expect("Device lookup failed") {
+        println!("Device {i}: {:?}", device.name);
+        devices[i] = device;
+        i = i + 1;
+    }
 
-    let mut cap = Capture::from_device(device)
-        .unwrap()
-        .immediate_mode(true)
-        .open()
-        .unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("Using device {:?}", devices[input]);
 }
