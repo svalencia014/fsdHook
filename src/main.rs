d36c62d4cc57f4ee39d02a0bd581444b67b82a1f
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-05-23 07:55:51 -0400

    capture works baby

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{io, any};
 use pcap::{Device, Capture};
 
 fn main() {
-    //find all devices installed
+    //  find all devices installed
     let mut devices = Vec::new();
     let mut i: u32 = 0;
     println!("Detected Devices:");
@@ -14,15 +14,21 @@ fn main() {
     println!("Select a device:");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
-    let device = devices.get(input.trim().parse::<usize>().unwrap()).unwrap();
+    let device: Device = devices.get(input.trim().parse::<usize>().unwrap()).unwrap().clone();
+    std::mem::drop(devices);
     println!("Using device: {:?}", device.name);
-
+    println!("Starting Capture");
     //setup capture
-    let mut cap = Capture::from_device(device)
-        .unwrap()
-        .immediate_mode(true)
-        .open()
-        .unwrap();
-
-    println!("{:?}", cap.next_packet());
+    let mut cap = Capture::from_device(device.clone())
+    .unwrap()
+    .immediate_mode(true)
+    .open()
+    .unwrap();
+    //filter non fsd packets
+    cap.filter("tcp port 6809", true).unwrap();
+    loop {
+        //  * Print packet for Debug Purposes
+        //  TODO: Remove before prod
+        println!("{:?}", cap.next_packet());
+    }
 }
